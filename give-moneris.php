34a74c35dccdc2c5d21ed11a2dbc348e6f741791
mehul0810 <mehul.gohil0810@gmail.com>
2018-12-14 13:26:23 +0530

    feat: include the admin filters file

--- give-moneris.php
+++ give-moneris.php
@@ -135,6 +135,7 @@ final class Give_Moneris {
 	public function includes() {
 
 		require_once GIVE_MONERIS_PLUGIN_DIR . '/includes/admin/class-give-moneris-admin-settings.php';
+		require_once GIVE_MONERIS_PLUGIN_DIR . '/includes/admin/admin-filters.php';
 		require_once GIVE_MONERIS_PLUGIN_DIR . '/includes/class-moneris-api.php';
 
 	}
