2925c01116e7e7df95261b5e33ee861e56915572
mehul0810 <mehul.gohil0810@gmail.com>
2018-12-14 13:21:36 +0530

    feat: add new file to add filters for admin

---
+++ includes/admin/admin-filters.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Admin Filters
+ *
+ * @package   Give-Moneris
+ * @copyright Copyright (c) 2018, WordImpress
+ * @license   https://opensource.org/licenses/gpl-license GNU Public License
+ * @since     1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	 exit;
+}
+
+/**
+ * Register the Moneris payment gateway.
+ *
+ * @access public
+ * @since  1.0.0
+ *
+ * @param array $gateways List of registered gateways.
+ *
+ * @return array
+ */
+function give_moneris_register_gateway( $gateways ) {
+
+	$gateways['moneris'] = array(
+		'admin_label'    => __( 'Moneris - Credit Card', 'give-moneris' ),
+		'checkout_label' => __( 'Credit Card', 'give-moneris' ),
+	);
+
+	return $gateways;
+
+}
+
+add_filter( 'give_payment_gateways', array( $this, 'give_moneris_register_gateway' ) );
