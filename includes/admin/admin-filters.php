6e6ddeac0625f054b3f95c36e056e529811b70dd
mehul0810 <mehul.gohil0810@gmail.com>
2019-01-16 10:21:59 +0530

    fix: add moneris settings page link in plugins listing

--- includes/admin/admin-filters.php
+++ includes/admin/admin-filters.php
@@ -35,3 +35,27 @@ function give_moneris_register_gateway( $gateways ) {
 }
 
 add_filter( 'give_payment_gateways', 'give_moneris_register_gateway' );
+
+/**
+ * Plugins row action links
+ *
+ * @since 1.0.0
+ *
+ * @param array $actions An array of plugin action links.
+ *
+ * @return array An array of updated action links.
+ */
+function give_moneris_plugin_action_links( $actions ) {
+	$new_actions = array(
+		'settings' => sprintf(
+			'<a href="%1$s">%2$s</a>',
+			admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways&section=moneris-settings' ),
+			__( 'Settings', 'give-moneris' )
+		),
+	);
+
+	return array_merge( $new_actions, $actions );
+}
+
+add_filter( 'plugin_action_links_' . GIVE_MONERIS_PLUGIN_BASENAME, 'give_moneris_plugin_action_links' );
+
