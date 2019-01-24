5b0e2433853779ae129b2afa9b2aa9c8586e4c00
Devin Walker <sdtucson@gmail.com>
2019-01-23 20:56:10 -0800

    fix: prevent JS error when refunding non-moneris payments

--- assets/src/js/admin/give-moneris-admin.js
+++ assets/src/js/admin/give-moneris-admin.js
@@ -1,18 +1,24 @@
 document.addEventListener( 'DOMContentLoaded', () => {
 
-    const donationStatus = document.getElementById( 'give-payment-status' );
+	const donationStatus = document.getElementById( 'give-payment-status' );
 
-    if ( null !== donationStatus ) {
-        donationStatus.addEventListener( 'change', ( event ) => {
+	if ( null !== donationStatus ) {
+		donationStatus.addEventListener( 'change', ( event ) => {
 
-            document.getElementById( 'give-moneris-opt-refund' ).checked = false;
+			let monerisRefundCheckbox = document.getElementById( 'give-moneris-opt-refund' );
 
-            // If donation status is complete, then show refund checkbox
-            if ( 'refunded' === event.target.value ) {
-                document.getElementById( 'give-moneris-opt-refund-wrap' ).style.display = 'block';
-            } else {
-                document.getElementById( 'give-moneris-opt-refund-wrap' ).style.display = 'none';
-            }
-        });
-    }
-});
+			if ( null === monerisRefundCheckbox) {
+				return;
+			}
+
+			monerisRefundCheckbox.checked = false;
+
+			// If donation status is complete, then show refund checkbox
+			if ( 'refunded' === event.target.value ) {
+				document.getElementById( 'give-moneris-opt-refund-wrap' ).style.display = 'block';
+			} else {
+				document.getElementById( 'give-moneris-opt-refund-wrap' ).style.display = 'none';
+			}
+		} );
+	}
+} );
